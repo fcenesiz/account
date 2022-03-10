2b43b340810ec964664a3801164484dcb053ea73
Fatih <ahmetfatihcenesizdev@gmail.com>
2022-03-10 18:15:26 +0300

    validation tested

--- src/main/kotlin/com/fcenesiz/account/controller/AccountController.kt
+++ src/main/kotlin/com/fcenesiz/account/controller/AccountController.kt
@@ -8,15 +8,16 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/v1/account")
 class AccountController(
     val accountService: AccountService
 ){
-
+    //@Valid ile CreateAccountRequest sınıfındaki @field:NotBlank ve @field:Min(0) kontrolü sağlandı.
     @PostMapping
-    fun createAccount(@RequestBody request: CreateAccountRequest): ResponseEntity<AccountDto> =
+    fun createAccount(@Valid @RequestBody request: CreateAccountRequest): ResponseEntity<AccountDto> =
         ResponseEntity.ok(accountService.createAccount(request))
 
 }
--- src/main/kotlin/com/fcenesiz/account/dto/CreateAccountRequest.kt
+++ src/main/kotlin/com/fcenesiz/account/dto/CreateAccountRequest.kt
@@ -1,8 +1,13 @@
 package com.fcenesiz.account.dto
 
 import java.math.BigDecimal
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
 
-class CreateAccountRequest (
+class CreateAccountRequest(
+
+    @field:NotBlank() // boş olmasını istemiyoruz, bilgisayarın diline göre default mesaj üretecektir. Yada kendimiz yazabiliriz.
     val customerId: String,
+    @field:Min(0) // negatif olamaz, bilgisayarın diline göre default mesaj üretecektir. Yada kendimiz yazabiliriz.
     val initialCredit: BigDecimal
-    )
+)
